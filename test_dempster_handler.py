61fb71cad28e7594960fd39d7bc19c2757e66aaa
fabi1628 <fabian.kueting@gmail.com>
2021-11-29 17:45:58 +0100

    tried to add tests

--- test_dempster_handler.py
+++ test_dempster_handler.py
@@ -1,4 +1,4 @@
-from python_evidence.dempster_handler import DempsterHandler, Measure
+from python_evidence.dempster_handler import DempsterHandler, Measure, _MeasureCollector
 
 """
 GOOD
@@ -12,7 +12,17 @@ def test_dempster_handler_2_good():
     dh = DempsterHandler()
     dh.add_categories(["t", "r", "s", "u", "e", "f"])
     dh.add_measure([Measure(["t", "r", "s"], 0.88)])
+    dh.add_measure([Measure(["e", "f"], 0.1)])
+    measures = dh.accumulate_all_measures()
 
+
+def test_dempster_handler_3_good():
+    dh = DempsterHandler()
+    measure_one = Measure(["r", "s"], 0.187)
+    measure_two = Measure(["u", "e"], 0.25)
+    ret = measure_one.__eq__(measure_two)
+    if ret:
+        print("wrong")
 
 
 """
